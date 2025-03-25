d5d7072c9968ed7a72223982ed25238e673835e2
Prince Bhatt <princebhatt316@gmail.com>
2025-03-25 12:18:43 +0530

    Bug Fixed, Folder Structure Crerated, Express App Created with the Express Generator

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,4 @@ router.get('/', function (req, res, next) {
   res.send('Hello, This is Home Page');
 });
 
-router.get('/UserRegister', function (req, res, next) {
-  res.send('Hello, This is User Register Page');
-});
-
 module.exports = router;
